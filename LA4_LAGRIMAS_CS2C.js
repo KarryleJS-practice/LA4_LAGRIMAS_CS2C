589c62260f2b27617333543457a3431e5f9d4221
KarryleJS-practice <karrylelagrimas6@gmail.com>
2024-09-23 10:51:18 +0800

    Create LA4_LAGRIMAS_CS2C.js

---
+++ LA4_LAGRIMAS_CS2C.js
@@ -0,0 +1,12 @@
+let favActorFirstName = "Kathryn";
+let favActorLastName = "Bernardo";
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase ();
+let message = "My favorite Actor is " + uppercase + ". She is a talented and beautiful actor.";
+
+message += " She is a brave women.";
+
+console.log("favActorLastName:", favActorLastName);
+console.log("fullName:", fullName) ;
+console.log("uppercase:", uppercase);
+console. log("message:", message);
